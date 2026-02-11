71fdd9dcb47984f977d94977270858472348cb79
MadBrozzeR <git.public.s@madbr.ru>
2026-02-11 16:47:31 +0300

    fix StreamInfo update

--- src/client/main.ts
+++ src/client/main.ts
@@ -4,7 +4,7 @@ import { ChatBox } from './components/chat-box';
 import { NotificationBox } from './components/notification-box';
 import { Audio } from './components/audio';
 import { urlState } from './utils/url-state';
-import { createCast } from './utils/broadcaster';
+import { createCast, isCast } from './utils/broadcaster';
 import { useModuleManager } from './utils/utils';
 import { Frame } from './components/frame';
 import { Countdown } from './components/countdown';
@@ -68,6 +68,12 @@ Splux.start(function (body, head) {
   urlState.listen(function (data) {
     moduleManager(data);
     host.cast('hashStateChange', data);
+  });
+
+  this.tuneIn(function (cast) {
+    if (isCast('streamInfo', cast)) {
+      host.state.streamInfo.set(cast.payload);
+    }
   });
 }, newHost());
 
